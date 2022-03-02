792dd25052bc76b71be311838527fed742b24f83
Ahbab1971 <mdahbab52@gmail.com>
2022-03-03 00:38:21 +0600

    deposit balance added balance account

--- js/banking.js
+++ js/banking.js
@@ -12,9 +12,25 @@ document.getElementById('deposit-submit').addEventListener('click', ()=>{
     
     const newdepositTotal = previousdepositTotal + newdepositAmount;
     depositTotal.innerText = newdepositTotal;
+    
+    // update account balance 
 
+    const balanceTotal = document.getElementById('balance-total');
+    const preBalanceText = balanceTotal.innerText;
+    const preBalanceTotal = parseFloat(preBalanceText);
+    const newBalanceTotal = preBalanceTotal + newdepositAmount;
 
-
-
+    balanceTotal.innerText = newBalanceTotal;
+  
     depositInput.value = '';
+
+    //
+
+   document.getElementById('withdraw-submit').addEventListener('click', ()=>{
+       console.log('click you')
+   })
+
+
+
+
 })
